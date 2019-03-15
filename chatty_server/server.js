b204deefbc2980ca2433b30428a7cfc8c6389371
fatuma ismail <timaismail90@gmail.com>
2019-03-15 20:31:34 +0000

    fixed indentation and cleaned the code

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -2,16 +2,15 @@ const express = require('express');
 const SocketServer = require('ws').Server;
 const uuid = require('uuid')
 
-// Set the port to 3001
 const PORT = 3001;
 
 // Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
+   // Made the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-// Create the WebSockets server
+// Created the WebSockets server
 const wss = new SocketServer({ server });
 
 // Set up a callback that will run when a client connects to the server
